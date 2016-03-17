7b4b15824f885901bee4df429987daa5ca243946
ldocao <ldocao@quantmetry.com>
2016-03-17 15:41:13 +0100

    feat: add config pythonpath

---
+++ config/pythonpath.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+export PYTHONPATH=$PYTHONPATH:"/Users/ldocao/Google Drive/Documents/Personnel/Administratifs/Logements/2016 03 Paris Jaures/rentabilite_appartement/rentabilite_appartement/app/"
