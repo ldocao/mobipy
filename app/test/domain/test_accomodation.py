7cb959e0c75f5075078fdf2fed8531c632e4259e
ldocao <ldocao@quantmetry.com>
2016-03-17 15:40:50 +0100

    test: add test for auto incrementation of accomodation counter

--- app/test/domain/test_accomodation.py
+++ app/test/domain/test_accomodation.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+import domain.accomodation as accomodation
+
+
+def test_incrementation_counter():
+    apartment1 = accomodation.Apartment()
+    apartment2 = accomodation.Apartment()
+    assert apartment1._id == 1
+    assert apartment2._id == 2
