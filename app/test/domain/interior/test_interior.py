c9050e6a86c730066dab87d809a1c01d41cd90b8
ldocao <ldocao@quantmetry.com>
2016-03-17 17:54:40 +0100

    feat: test all rooms, and check that sum of habitable room is ok

--- app/test/domain/interior/test_interior.py
+++ app/test/domain/interior/test_interior.py
@@ -3,7 +3,7 @@
 import pytest
 from domain.interior.interior import Interior, Caracteristics, Surface
 from domain.accomodation import Apartment
-from domain.interior.room import Kitchen, MainRoom
+from domain.interior.room import Kitchen, MainRoom, Lavatory, LivingRoom, BedRoom
 
 
 @pytest.fixture
@@ -12,5 +12,14 @@ def studio():
                             [Kitchen(Surface(10)), MainRoom(Surface(10))])
     return studio
 
+@pytest.fixture
+def room3():
+    room3 =  Caracteristics(Surface(20),
+                            [Kitchen(Surface(10)), MainRoom(Surface(10)), Lavatory(Surface(2)), LivingRoom(Surface(23)), BedRoom(Surface(23))])
+    return room3
+
 def test_number_of_habitable_room(studio):
     assert Caracteristics.number_of_habitable_room(studio) == 1
+
+def test_number_of_habitable_room2(room3):
+    assert Caracteristics.number_of_habitable_room(room3) == 3
