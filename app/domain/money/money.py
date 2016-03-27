7835e5b660437a515b4a279da3f018261f3b6263
ldocao <ldocao@quantmetry.com>
2016-03-27 19:23:42 +0200

    feat: define __add__ method

--- app/domain/money/money.py
+++ app/domain/money/money.py
@@ -6,7 +6,15 @@ class Money():
         self.currency = currency
 
     def __add__(self, other):
+        from domain.money.currency import EURO
+        
         if self.currency == other.currency:
             sum_value = self.value + other.value
             return Money(sum_value, self.currency)
+        else:
+            sum_value_in_euro = self.value_in_euro() + other.value_in_euro()
+            return Money(sum_value_in_euro, EURO)
         
+
+    def value_in_euro(self):
+        return self.value * self.currency.unit_value_in_euro
